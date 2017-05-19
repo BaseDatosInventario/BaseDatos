e3c7fccf0da2c59db606bbbc5fdcb7efe9446a78
Pedrito Alejos <pedroalejosb@gmail.com>
2017-05-18 21:50:55 -0400

    Se crearon los atributos de las tablas

--- inventario.sql
+++ inventario.sql
@@ -1,13 +1,30 @@
 
 create table detalle_venta (
+id_venta int ,
+id_producto int,
+cantidad_producto int ,
+total double
 
 );
 
 create table caja (
+id_caja int primary key ,
+fecha_caja date,
+monto_inicial double,
+monto_actual double,
+observacion text 
 
 );
 
 create table venta (
-
+id_venta int ,
+fecha date,
+total_Venta double,
+descuento double,
+observacion text,
+monto_pago double,
+monto_vuelto double,
+regalo text,
+forma_pago varchar (30)
 
 );

b60354bda6bfba75719cf1069efd72070adbb57a
Grinsick <rodrigo-lopez1996@hotmail.com>
2017-05-18 21:40:22 -0400

    se creo la tabla usuario

--- inventario.sql
+++ inventario.sql
@@ -9,3 +9,11 @@ create table funci_permiso
 num_run_f int not null,
 id_permiso int not null, 
 )
+
+create table usuario
+(
+num_run_f int not null,
+clave varchar(16) not null,
+id_tipo_usuario int not null,
+estado varchar(30) not null
+)

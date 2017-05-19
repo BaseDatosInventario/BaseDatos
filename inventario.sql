214509fe003770858adf4a35c144c3d83be8367e
Grinsick <rodrigo-lopez1996@hotmail.com>
2017-05-18 22:03:46 -0400

    organizacion del codigo

--- inventario.sql
+++ inventario.sql
@@ -8,36 +8,40 @@ p_apellido varchar(30) not null,
 s_apellido varchar(30) not null,
 telefono int not null,
 direccion varchar(60) not null 
-)
-
-create table tipo_usuario
-(
-id_tipo_usuario int primary key,
-descripcion varchar(30)
 );
+
+
 
 
 create table usuario
 (
-num_run_f int not null,
+num_run_f int not null PRIMARY key,
 clave varchar(16) not null,
 id_tipo_usuario int not null,
 estado varchar(30) not null
-)
-alter table usuario add FOREIGN KEY (num_run_f) REFERENCES funcionario(num_run) 
+);
+
+create table tipo_usuario
+(
+id_tipo_usuario int AUTO_INCREMENT primary key not null,
+descripcion varchar(30) not null
+);
+
+
 
 
 create table usuario_permiso
 (
 num_run_u int not null,
-id_permiso int not null, 
-)
-alter table usuario_permiso add FOREIGN KEY (num_run_u) REFERENCES usuario(num_run_f) 
-alter table usuario_permiso add FOREIGN KEY (id_permiso) REFERENCES permiso(id_permiso) 
-
+id_permiso int not null
+);
 
 create table permiso
 (
 id_permiso int primary key,
 descripcion varchar(30) not null
-)
+);
+alter table usuario_permiso add FOREIGN KEY (num_run_u) REFERENCES usuario(num_run_f); 
+alter table usuario_permiso add FOREIGN KEY (id_permiso) REFERENCES permiso(id_permiso); 
+alter table usuario add FOREIGN KEY (num_run_f) REFERENCES funcionario(num_run); 
+alter table usuario add FOREIGN key (id_tipo_usuario) REFERENCES tipo_usuario(id_tipo_usuario)

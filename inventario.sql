1ab60587a618c59103dc3a56e4c934267e79f2b9
Pedrito Alejos <pedroalejosb@gmail.com>
2017-05-18 22:23:36 -0400

    Se crearon las llaves foraneas respectivas

--- inventario.sql
+++ inventario.sql
@@ -1,5 +1,5 @@
 create table Producto (
-id_producto primary key ,
+id_producto int primary key  ,
 nombre varchar (50),
 detalle text,
 stock_min int,
@@ -10,39 +10,39 @@ fecha_ingreso date,
 estado char(2),
 id_tipo_producto int 
 
+
 );
 
+alter table Producto add FOREIGN KEY (id_tipo_producto) REFERENCES Tipo_producto(id_tipo_producto);
+
+
 create table Tipo_producto(
-id_tipo_producto int primary key,
+id_tipo_producto int AUTO_INCREMENT  primary key  ,
 nombre varchar (50),
 detalle text
 );
 create table Merma(
-id_merma primary key ,
+id_merma int AUTO_INCREMENT primary key ,
 id_producto int 
 cantidad_product int ,
 costo_perdida double 
 );
+alter table Merma add FOREIGN KEY (id_producto) REFERENCES Producto(id_producto);
+
 
 create table Detalle_venta (
 id_venta int ,
 id_producto int,
 cantidad_producto int ,
 total double
-alter table producto add FOREIGN KEY (num_run_f) REFERENCES funcionario(num_run) 
-);
-
-create table Caja (
-id_caja int primary key ,
-fecha_caja date,
-monto_inicial double,
-monto_actual double,
-observacion text 
 
 );
+
+alter table Detalle_venta add FOREIGN KEY (id_producto) REFERENCES Producto(id_producto);
+alter table Detalle_Venta add FOREIGN KEY (id_venta) REFERENCES Venta(id_venta);
 
 create table Venta (
-id_venta int ,
+id_venta int AUTO_INCREMENT primary key ,
 fecha date,
 total_Venta double,
 descuento double,
@@ -53,3 +53,14 @@ regalo text,
 forma_pago varchar (30)
 
 );
+
+
+create table Caja (
+id_caja int primary key ,
+fecha_caja date,
+monto_inicial double,
+monto_actual double,
+observacion text 
+
+);
+

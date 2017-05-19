5b485b7534551fb4ec42b75c5820e503c11f5418
Grinsick <rodrigo-lopez1996@hotmail.com>
2017-05-18 21:47:41 -0400

    se crearon las tablas faltantes de parte del funcionario

--- inventario.sql
+++ inventario.sql
@@ -1,14 +1,20 @@
+
+create table funcionario
+(
+num_run int primary key,
+dv_run char(1) not null,
+p_nombre varchar(30) not null,
+p_apellido varchar(30) not null,
+s_apellido varchar(30) not null,
+telefono int not null,
+direccion varchar(60) not null 
+)
+
 create table tipo_usuario
 (
 id_tipo_usuario int primary key,
 descripcion varchar(30)
 );
-
-create table funci_permiso
-(
-num_run_f int not null,
-id_permiso int not null, 
-)
 
 create table usuario
 (
@@ -16,4 +22,18 @@ num_run_f int not null,
 clave varchar(16) not null,
 id_tipo_usuario int not null,
 estado varchar(30) not null
+)
+
+
+
+create table usuario_permiso
+(
+num_run_u int not null,
+id_permiso int not null, 
+)
+
+create table permido
+(
+id_permiso int primary key,
+descripcion varchar(30) not null
 )

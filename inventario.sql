375ede350539efba0b507f7c917ce135afef0baf
Pedrito Alejos <pedroalejosb@gmail.com>
2017-05-18 22:03:04 -0400

    Se creo la tabla producto,tipo_producto y merma

--- inventario.sql
+++ inventario.sql
@@ -1,13 +1,38 @@
+create table Producto (
+id_producto primary key ,
+nombre varchar (50),
+detalle text,
+stock_min int,
+stock_max int ,
+valor_venta double,/*valor al que se vende el producto*/
+valor_costo double, /*valor al que se compra al provedor*/
+fecha_ingreso date,
+estado char(2),
+id_tipo_producto int 
 
-create table detalle_venta (
+);
+
+create table Tipo_producto(
+id_tipo_producto int primary key,
+nombre varchar (50),
+detalle text
+);
+create table Merma(
+id_merma primary key ,
+id_producto int 
+cantidad_product int ,
+costo_perdida double 
+);
+
+create table Detalle_venta (
 id_venta int ,
 id_producto int,
 cantidad_producto int ,
 total double
-
+alter table producto add FOREIGN KEY (num_run_f) REFERENCES funcionario(num_run) 
 );
 
-create table caja (
+create table Caja (
 id_caja int primary key ,
 fecha_caja date,
 monto_inicial double,
@@ -16,7 +41,7 @@ observacion text
 
 );
 
-create table venta (
+create table Venta (
 id_venta int ,
 fecha date,
 total_Venta double,
